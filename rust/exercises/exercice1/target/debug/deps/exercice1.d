48795b42373408d4fe855139032a51b2b51e0ef0
Valentin Massonnière <81711481+TheValll@users.noreply.github.com>
2026-04-09 21:37:08 +0200

    Delete rust/exercises directory

--- rust/exercises/exercice1/target/debug/deps/exercice1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Valentin\dev\wiki\rust\exercises\exercice1\target\debug\deps\exercice1.d: src\main.rs
-
-C:\Users\Valentin\dev\wiki\rust\exercises\exercice1\target\debug\deps\exercice1.exe: src\main.rs
-
-src\main.rs:
--- rust/exercises/exercice1/target/debug/exercice1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Valentin\dev\wiki\rust\exercises\exercice1\target\debug\exercice1.exe: C:\Users\Valentin\dev\wiki\rust\exercises\exercice1\src\main.rs
